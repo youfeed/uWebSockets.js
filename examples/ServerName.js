96adc1adaca76727c92e139f04ead9adafc47a11
张小路 <youfeed@qq.com>
2024-11-11 06:50:29 +0800

    Update ServerName.js

--- examples/ServerName.js
+++ examples/ServerName.js
@@ -1,6 +1,7 @@
 /* Minimal SSL example using ServerNameIndication */
 
-const uWS = require('../dist/uws.js');
+// npm install uNetworking/uWebSockets.js#latest package.json->type:module
+import uWS from 'uWebSockets.js';
 const port = 9001;
 
 const app = uWS.SSLApp({
