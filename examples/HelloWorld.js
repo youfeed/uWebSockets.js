76f84230f406558ffe8634f109b08419e5c7e9d1
杰克布达 <11247005@qq.com>
2024-11-11 07:21:11 +0800

    Update HelloWorld.js

--- examples/HelloWorld.js
+++ examples/HelloWorld.js
@@ -1,6 +1,7 @@
 /* Minimal SSL/non-SSL example */
 
-const uWS = require('../dist/uws.js');
+// npm install uNetworking/uWebSockets.js#latest package.json->type:module
+import uWS from 'uWebSockets.js';
 const port = 9001;
 
 const app = uWS./*SSL*/App({
