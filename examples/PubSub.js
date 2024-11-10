248cee6383dcfe8f744439edc84a3d99f21ea8f7
张小路 <youfeed@qq.com>
2024-11-11 06:50:02 +0800

    Update PubSub.js

--- examples/PubSub.js
+++ examples/PubSub.js
@@ -1,6 +1,6 @@
 /* Simple pub/sub example (WIP) */
-
-const uWS = require('../dist/uws.js');
+// npm install uNetworking/uWebSockets.js#latest package.json->type:module
+import uWS from 'uWebSockets.js';
 const port = 9001;
 
 const app = uWS./*SSL*/App({
