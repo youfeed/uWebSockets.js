6daf7ef5c1ac96082a96d005581d845734b21624
张小路 <youfeed@qq.com>
2024-11-11 06:48:35 +0800

    Update H3lloWorld.js

--- examples/H3lloWorld.js
+++ examples/H3lloWorld.js
@@ -1,6 +1,7 @@
 /* Minimal HTTP/3 example */
 
-const uWS = require('../dist/uws.js');
+// npm install uNetworking/uWebSockets.js#latest package.json->type:module
+import uWS from 'uWebSockets.js';
 const port = 9001;
 
 /* ./quiche-client --no-verify https://localhost:9001 */
