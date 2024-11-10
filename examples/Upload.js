f597f9924dbf1838e4595ac2742ab5a0b25161ac
杰克布达 <11247005@qq.com>
2024-11-11 07:23:36 +0800

    Update Upload.js

--- examples/Upload.js
+++ examples/Upload.js
@@ -4,7 +4,8 @@
 /* a good example would be using the router to get
  * a file name and stream that file to disk */
 
-const uWS = require('../dist/uws.js');
+// npm install uNetworking/uWebSockets.js#latest package.json->type:module
+import uWS from 'uWebSockets.js';
 const port = 9001;
 
 const app = uWS./*SSL*/App({
